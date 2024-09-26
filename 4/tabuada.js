0d323ac62c019b26b20d519291f09620058ec2a9
elissonguerra <elisson.guerra@gmail.com>
2024-09-26 20:37:42 -0300

    Adicionado a lista de exercícios e o enunciado deles

---
+++ 4/tabuada.js
@@ -0,0 +1,10 @@
+var numero = parseInt(prompt("Digite um número inteiro para verficar a tabuada"));
+
+var resultado = 0;
+
+for (let i = 0; index <= 10; index++) {
+    resultado += `${numero} x ${i}` = ${numero * i};   
+}
+
+console.log(resultado);
+
