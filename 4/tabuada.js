5b4dd1c0b4fe3b2a3f8a9eff1962324a8203276a
elissonguerra <elisson.guerra@gmail.com>
2024-09-30 20:31:51 -0300

    Finalizado os exercícios e adicionado bootstrap

--- 4/tabuada.js
+++ 4/tabuada.js
@@ -1,10 +1,14 @@
-var numero = parseInt(prompt("Digite um número inteiro para verficar a tabuada"));
-
-var resultado = 0;
-
-for (let i = 0; index <= 10; index++) {
-    resultado += `${numero} x ${i}` = ${numero * i};   
+function mostrarTabuada() {
+    const numero = prompt("Digite um número para ver a tabuada:");
+    
+    if (numero !== null && !isNaN(numero)) {
+        console.log(`Tabuada do ${numero}:`);
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${numero} x ${i} = ${numero * i}`);
+        }
+    } else {
+        console.log("Por favor, insira um número válido.");
+    }
 }
 
-console.log(resultado);
-
+mostrarTabuada();
